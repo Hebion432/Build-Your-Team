2284b9f44989b8e4503ad426fd9e6f5cc8e5018c
Amit <learnniggo981@gmail.com>
2025-06-05 23:09:46 +0530

    Made header component and clean up folder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./component/Header/header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      Hello From Amit
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Header/header.js
@@ -0,0 +1,11 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <div class="header">
+      <h1>Choose Your team</h1>
+    </div>
+  );
+}
+
+export default Header;
